d42ea0f98ab69c3b30f22dc3dad2e1091eaebf13
jefflunt <jefflunt@gmail.com>
2023-01-24 19:36:03 -0600

    add a bunch of modes

--- lib/tiny_color.rb
+++ lib/tiny_color.rb
@@ -1,57 +1,72 @@
-# this code extends the String class, adding ANSI color codes for modifying the
-# text.
+# this class extends the String class to add color formatting methods using
+# ANSI escape codes.
 #
-# usage:
-#   puts 'Hi'.green + ' there!'
+# try these examples:
+#   require 'tiny_color'
 #
-#   "Hi there!"                             # where 'Hi' will appear in green,
-#                                           # and the rest in your default #
-#                                           # terminal color
+#   "Hello World!".red            # "Hello World!" in red
+#   "Hello World!".on_green       # "Hello World!" with green background
+#   "Hello World!".bold           # "Hello World!" in bold
+#   "Hello World!".underline      # "Hello World!" with underline
+#   "Hello World!".red.underline  # "Hello World!" with red color and underline
 #
-# you can also add background colors:
-#   puts 'Hi'.black.on_yellow + ' there!'
-#
-#   "Hi there!"                             # where 'Hi' will appear in black
-#                                           # with a yellow background, and the
-#                                           # rest in your default terminal
-#                                           # color
+# for support notes, see: https://en.wikipedia.org/wiki/ANSI_escape_code
 class String
   COLORS = {
     black:          30,
-    light_black:    90,
-
-    grey:           90,
-    gray:           90,
-
+    gray:           90, # convenient alias for light_black
+    grey:           90, # convenient alias for light_black
     red:            31,
-    light_red:      91,
-
     green:          32,
-    light_green:    92,
-
     yellow:         33,
-    light_yellow:   93,
-
     blue:           34,
-    light_blue:     94,
-
     purple:         35,
-    light_purple:   95,
-
     cyan:           36,
-    light_cyan:     96,
-
     white:          37,
+    light_black:    90,
+    light_red:      91,
+    light_green:    92,
+    light_yellow:   93,
+    light_blue:     94,
+    light_purple:   95,
+    light_cyan:     96,
     light_white:    97
+  }
+
+  # see: https://en.wikipedia.org/wiki/ANSI_escape_code
+  # not all of these modes may be supported by your terminal
+  #
+  # some terminal emulators require that you enable certain features, such as
+  # blinking text
+  MODES = {
+    normal:           0,
+    bold:             1,
+    dim:              2,
+    italic:           3,
+    underline:        4,
+    blink:            5,
+    blink_slow:       5,
+    blink_fast:       6,
+    invert:           7,
+    hide:             8,
+    strike:           9,
+    double_underline: 20,
+    reveal:           28,
+    overlined:        53
   }
 
   COLORS.each do |color, value|
     define_method color do
       "\033[#{value}m#{self}\033[0m"
     end
-
     define_method "on_#{color}" do
       "\033[#{value + 10}m#{self}\033[0m"
+    end
+  end
+
+  MODES.each do |mode, value|
+    define_method mode do
+      "\033[#{value}m#{self}\033[0m"
     end
   end
 end
--- tiny_color.gemspec
+++ tiny_color.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = "tiny_color"
-  s.version     = "1.1.0"
+  s.version     = "1.2.0"
   s.description = "a tiny library for String colorization"
   s.summary     = "this is a minimalist library for adding color to Strings by extending the String class"
   s.authors     = ["Jeff Lunt"]
